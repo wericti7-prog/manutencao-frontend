21bc75f64a1d0aba225e2cfa49076984eb00e9bf
wericsimoes7 <wericti7@gmail.com>
2026-08-11 16:25:25 -0300

    Aumentar tamanho das imagens de equipamentos6

--- script.js
+++ script.js
@@ -1890,7 +1890,7 @@ window.abrirMovimentoEstoque = function(id, tipo) {
     document.getElementById("estoqueMovTipo").value    = tipo;
     document.getElementById("estoqueMovQuantidade").value = 1;
     document.getElementById("modalEstoqueMovimentoTitulo").textContent =
-        (tipo === "entrada" ? "⬆️ Entrada — " : "⬇️ Saída — ") + item.nome;
+        (tipo === "entrada" ? "⇅ Entrada — " : "⇅ Saída — ")
     document.getElementById("estoqueMovDisponivel").textContent =
         `Disponível atualmente: ${item.quantidade} ${item.unidade || "un"}`;
     openModal("modalEstoqueMovimento");
